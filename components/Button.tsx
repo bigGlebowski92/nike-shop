dde4eebe0d20c3d9798fb0ea4b1abf88c09156c8
bigGlebowski <glibkorniienko92@gmail.com>
2023-08-27 11:29:28 +0300

    added functionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,20 +3,22 @@ import React from 'react';
 
 type ButtonProps = {
   label: string;
-  iconURL: string;
+  iconURL?: string;
 };
 
 const Button = ({ label, iconURL }: ButtonProps) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <Image
-        src={iconURL}
-        alt="button-icon"
-        width={20}
-        height={20}
-        className="ml-2 rounded-full"
-      />
+      {iconURL && (
+        <Image
+          src={iconURL}
+          alt="button-icon"
+          width={20}
+          height={20}
+          className="ml-2 rounded-full"
+        />
+      )}
     </button>
   );
 };
--- components/Services.tsx
+++ components/Services.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      Services
+    </section>
+  );
 };
 
 export default Services;
